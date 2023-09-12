25e69c54f18f7a92c169cb77bda02ea51dcc87d6
Kamil Dominko <kamil.dominko@gmail.com>
2023-09-12 21:06:00 +0200

    Nadanie stylu punktowi.

--- scatter_squares.py
+++ scatter_squares.py
@@ -1,7 +1,18 @@
 import matplotlib.pyplot as plt
+from matplotlib.axes import Axes
 
-plt.style.use("ggplot")
+ax: Axes
+
+plt.style.use("seaborn")
 fig, ax = plt.subplots()
-ax.scatter(2, 4)
+ax.scatter(2, 4, s=200)
+
+# Zdefiniowanie tytułu wykresu i etykiet osi.
+ax.set_title("Kwadraty liczb", fontsize=24)
+ax.set_xlabel("Wartości", fontsize=24)
+ax.set_ylabel("Kwadraty wartości", fontsize=14)
+
+# Zdefiniowanie wielkości etykiet.
+ax.tick_params(axis="both", which="major", labelsize=14)
 
 plt.show()
