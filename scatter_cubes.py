a3a5d6f393d994543d2dec3906d32e938b92f068
Kamil Dominko <kamil.dominko@gmail.com>
2023-09-12 21:56:49 +0200

    Dodanie wykresu kwadratów i sześcianów z funkcją zapisania wykresu do pliku PNG.

---
+++ scatter_cubes.py
@@ -0,0 +1,27 @@
+import matplotlib.pyplot as plt
+from matplotlib.axes import Axes
+
+ax: Axes
+
+x_values = range(5001)
+y_values = [x ** 3 for x in x_values]
+
+plt.style.use("ggplot")
+fig, ax = plt.subplots()
+ax.scatter(x_values, y_values, c=y_values, cmap=plt.cm.Greens, s=10)
+
+# Zdefiniowanie tytułu wykresu i etykiet osi.
+ax.set_title("Sześciany liczb", fontsize=24)
+ax.set_xlabel("Wartości", fontsize=14)
+ax.set_ylabel("Sześciany wartości", fontsize=14)
+
+# Zdefiniowanie wielkości etykiet.
+ax.tick_params(axis="both", which="major", labelsize=14)
+
+# Zdefiniowanie zakresu dla każdej osi.
+ax.axis([0, 5100, 0, 135000000000])
+
+plt.show()
+
+# Zapisanie do pliku PNG.
+plt.savefig("cubes_plot.png", bbox_inches="tight")
--- scatter_squares_auto.py
+++ scatter_squares_auto.py
@@ -3,13 +3,13 @@ from matplotlib.axes import Axes
 
 ax: Axes
 
-x_values = [1, 2, 3, 4, 5]
-y_values = [1, 4, 9, 16, 25]
+x_values = range(1, 1001)
+y_values = [x ** 2 for x in x_values]
 
 plt.style.use("ggplot")
 fig, ax = plt.subplots()
-# ax.scatter(2, 4, s=200)
-ax.scatter(x_values, y_values, s=100)
+# ax.scatter(x_values, y_values, c="green", s=10)
+ax.scatter(x_values, y_values, c=y_values, cmap=plt.cm.Blues, s=10)
 
 # Zdefiniowanie tytułu wykresu i etykiet osi.
 ax.set_title("Kwadraty liczb", fontsize=24)
@@ -19,4 +19,10 @@ ax.set_ylabel("Kwadraty wartości", fontsize=14)
 # Zdefiniowanie wielkości etykiet.
 ax.tick_params(axis="both", which="major", labelsize=14)
 
+# Zdefiniowanie zakresu dla każdej osi.
+ax.axis([0, 1100, 0, 1100000])
+
 plt.show()
+
+# Zapisanie do pliku PNG.
+plt.savefig("squares_plot.png", bbox_inches="tight")
