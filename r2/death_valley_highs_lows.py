5dfb778ec53f1b27f16ea44a01a9b9a9fa9ff2a2
Kamil Dominko <kamil.dominko@gmail.com>
2023-10-03 12:39:28 +0200

    Pobieranie i wizualizacja danych.

--- r2/death_valley_highs_lows.py
+++ r2/death_valley_highs_lows.py
@@ -6,28 +6,35 @@ from matplotlib.figure import Figure
 
 ax: Axes
 fig: Figure
-
-filename = "data/sitka_weather_07-2018_simple.csv"
+filename = "data/death_valley_2018_simple.csv"
 with open(filename) as f:
     reader = csv.reader(f)
     header_row = next(reader)
 
     # Pobranie temperatur maksymalnych z pliku.
-    dates, highs = [], []
+    dates, highs, lows = [], [], []
     for row in reader:
         current_date = datetime.strptime(row[2], "%Y-%m-%d")
-        high = int(row[5])
-        dates.append(current_date)
-        highs.append(high)
-    print(highs)
+        try:
+            high = int(row[4])
+            low = int(row[5])
+        except ValueError:
+            print(f"Brak danych dla {current_date}.")
+        else:
+            dates.append(current_date)
+            highs.append(high)
+            lows.append(low)
 
 # Wygenerowanie wykresu najwyższych temperatur.
 plt.style.use("seaborn")
 fig, ax = plt.subplots()
-ax.plot(dates, highs, c="red")
+ax.plot(dates, highs, c="red", alpha=0.5)
+ax.plot(dates, lows, c="blue", alpha=0.5)
+ax.fill_between(dates, highs, lows, facecolor="blue", alpha=0.1)
 
 # Formatowanie wykresu.
-ax.set_title("Najwyższa temperatura dnia, lipiec 2018", fontsize=24)
+ax.set_title("Najwyższa i najniższa temperatura dnia 2018"
+             "\nDolina Śmierci, Kalifornia", fontsize=24)
 ax.set_xlabel("", fontsize=16)
 fig.autofmt_xdate()  # Wyświetlenie etykiet dat ukośnie.
 ax.set_ylabel("Temperatura (F)", fontsize=16)
